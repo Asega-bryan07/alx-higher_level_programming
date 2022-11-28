d9e7fe27939a76bed8e996031320175a5da36851
Asega-bryan07 <Almasibryan7@gmail.com>
2022-11-28 13:11:59 -0800

    python3

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -19,3 +19,5 @@ int check_cycle(listint_t *list);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
+#endif /* LISTS_H */
