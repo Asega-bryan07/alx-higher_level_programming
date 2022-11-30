1d372b337085d69631ab0ddb9bd68b16718e052e
Asega-bryan07 <Almasibryan7@gmail.com>
2022-11-30 05:55:05 -0800

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,4 +4,4 @@ for i in range(ord('z'), ord('a') - 1, -1):
         diff = 0
     else:
         diff = 32
-    print('{}'.format(char(i - diff)), end="")
+    print('{}'.format(chr(i - diff)), end="")
