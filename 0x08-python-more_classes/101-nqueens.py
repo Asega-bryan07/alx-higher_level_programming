de92eb75779edda959e22c83c7afefad7962538e
Asega-bryan07 <Almasibryan7@gmail.com>
2023-01-04 03:08:09 +0300

    More Classes Python3

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-
 """Solves the N-queens puzzle"""
 
 import sys
 
+
 def init_board(n):
-    """Initialize an `n `x`n` sized chessboard qith 0's"""
+    """Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
     [board.append([]) for i in range(n)]
-    [board.append(' ') for i in range(n) for row in board]
+    [row.append(' ') for i in range(n) for row in board]
     return (board)
 
 
 def board_deepcopy(board):
-    """Return a deepcopy of chessboard"""
+    """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
-        return (board)
+    return (board)
 
 
 def get_solution(board):
