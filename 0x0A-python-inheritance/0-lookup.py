03c9a10a0692dff3a97d1e50fcb0491eb8e2e41f
root <root@DESKTOP-Q1I9VLT.localdomain>
2023-01-10 12:02:21 +0300

    Python Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a module that returns the list of available attributes and methods of an object
+a module that returns the list of available attributes and methods
+of an object
 """
 
 def lookup(obj):
