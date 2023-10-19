bcc8619c6e0b99a63d6bb279cfc9e43d68659b00
Asega-bryan07 <Almasibryan7@gmail.com>
2023-10-19 19:07:34 +0300

    Advanced MySQL Queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,16 @@
+-- (same as 12-no_genre.sql)
+-- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- First column must be called genre
+-- Second column must be called number_of_shows
+-- Don’t display a genre that doesn’t have any shows linked
+-- Results must be sorted in descending order by the number of shows linked
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT g.`name` AS `genre`,
+COUNT (*) AS `number_of_shows`
+FROM tv_genres AS g
+INNER JOIN `tv_show_genre` AS t
+ON g.`id` = t.`genre_id`
+GROUP BY g.`name`
+ORDER BY `number_of_shows` DESC;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,19 @@
+More SQL Queries
+echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+service mysql start 
+How To Create a New User and Grant Permissions in MySQL
+How To Use MySQL GRANT Statement To Grant Privileges To a User
+MySQL constraints
+SQL technique: subqueries
+Basic query operation: the join
+SQL technique: multiple joins and the distinct keyword
+SQL technique: join types
+SQL technique: union and minus
+MySQL Cheat Sheet
+The Seven Types of SQL Joins
+MySQL Tutorial
+SQL Style Guide
+MySQL 8.0 SQL Statement Syntax
+
+-- A SQL QUERY WALKS INTO A BAR AND SEES TWO TABLES
+-- HE WALKS UP TO THEM AND SAYS, CAN I JOIN YOU?
