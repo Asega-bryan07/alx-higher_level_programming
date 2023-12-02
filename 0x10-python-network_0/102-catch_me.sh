ad62be5e71708f41212d65eff08702c52e9ce0c9
Asega-bryan07 <almasibryan7@gmail.com>
2023-12-02 10:38:52 -0800

    Python Network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -6,4 +6,4 @@
 # You are not allow to use echo, cat, etc. to display the final result
 # test your script in the sandbox provided, using the web server
 # running on port 5000
-curl -sL -X PUT -H "Origin: ALXSoftwareEngineering" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: ALXSoftwareEngineering" -d "user_id=98 0.0.0.0:5000/catch_me" "$1"
